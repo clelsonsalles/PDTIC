475328f07f2a80233f5762920a49af4708c6998d
clelsonsalles <clelson.salles@gmail.com>
2017-04-04 21:39:55 -0300

    Limpando arquivos antigos

--- dbChangeLog/31-08-2016.sql
+++
@@ -1,4 +0,0 @@
-/**
- Alteração do tipo da coluna "dstextomanifestacao" da tabela "tbmanifestacao" para o tipo Text.
- */
-ALTER TABLE tbmanifestacao ALTER COLUMN dstextomanifestacao text SET NOT NULL;
